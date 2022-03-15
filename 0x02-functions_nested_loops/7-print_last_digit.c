c06a65b37db2ccf9aeae77396d102aae9991edc2
CatherineOnye <catherineonye19@gmail.com>
2022-03-15 15:23:16 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,15 @@
 
 int print_last_digit(int n)
 {
-	int last = n % 10;
-
-	if (n < 0)
-		last = last * -1;
-
-	_putchar(last + '0');
-
-	return (last);
+	int lastDig;
+	if (n >= 0)
+	{
+		lastDig = n % 10;
+	}
+	else
+	{
+	lastDig = (n % 10) * -1;
+	}
+_putchar('0' + lastDig);
+return (lastDig);
 }
